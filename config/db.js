c5c9762a3ab218a4b3040740b2f74135848e8a9f
Evan J. Washington <enavy04@gmail.com>
2018-12-14 05:58:12 -0800

    updated config/db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
-var mongoose = require('mongoose');
-mongoose.Promise = Promise;
-var mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl, function(err){
-  if(err){
-    throw err;
-  }
-  console.log('database connected');
-});
+var mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+var mongoUrl = process.env.MONGODB_URI
 
-module.exports = mongoose;
+ mongoose.connect(mongoUrl, {
+  useMongoClient: true,
+  /* other options */
+})
+ .then(function(){
+ 	console.log('database connected')
+ })
+ .catch(function(err){
+    console.log(err)
+})
+
+module.exports = mongoose
